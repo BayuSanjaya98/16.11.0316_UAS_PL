70a704732a13731bd8b6f771f289b463aa051bb4
A S U S <A S U S@LAPTOP-J8OKCM3K>
2021-01-13 21:56:38 +0800

    Add project files.

---
+++ 16.11.0316_UAS_PL/Penawaran.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Promos.Controller;
+using Promos.Model;
+
+namespace Promos
+{
+    /// <summary>
+    /// Interaction logic for Penawaran.xaml
+    /// </summary>
+
+    public partial class Penawaran : Window
+    {
+        PenawaranController controller;
+        OnPenawaranChangedListener listener;
+        public Penawaran()
+        {
+            InitializeComponent();
+
+            controller = new PenawaranController();
+            listPenawaran.ItemsSource = controller.getItems();
+
+            generateContentPenawaran();
+
+        }
+
+        public void SetOnItemSelectedListener(OnPenawaranChangedListener listener)
+        {
+            this.listener = listener;
+        }
+
+        private void generateContentPenawaran()
+        {
+            Item drink1 = new Item("Coffe Late", 30000);
+            Item drink2 = new Item("Black Tea", 20000);
+            Item drink3 = new Item("Milk Shake", 15000);
+            Item drink4 = new Item("Watermelon Juice", 25000);
+            Item drink5 = new Item("Melon Squash", 30000);
+
+            Item food4 = new Item("Pizza", 75000);
+            Item food5 = new Item("Friend Frice Special", 45000);
+
+            controller.addItem(drink1);
+            controller.addItem(drink2);
+            controller.addItem(drink3);
+            controller.addItem(drink4);
+            controller.addItem(drink5);
+
+            controller.addItem(food4);
+            controller.addItem(food5);
+
+            listPenawaran.Items.Refresh();
+        }
+
+        private void listPenawaran_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBox listbox = sender as ListBox;
+            Item item = listbox.SelectedItem as Item;
+            Debug.WriteLine(item.title);
+
+            this.listener.onPenawaranSelected(item);
+        }
+    }
+
+    public interface OnPenawaranChangedListener
+    {
+        void onPenawaranSelected(Item item);
+    }
+}
